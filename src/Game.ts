3fa1728d5577f4307d8a5b6c6280875f09689043
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2021-03-23 11:44:12 +0000

    DRY => remove comments and duplication

--- src/Game.ts
+++ src/Game.ts
@@ -26,42 +26,40 @@ export class Game {
         this._board.AddTileAt(symbol, x, y);
     }
 
-    public Winner(): string {
-        //if the positions in first row are taken
-        if (this._board.TileAt(0, 0)!.Symbol !== ' ' &&
-            this._board.TileAt(0, 1)!.Symbol !== ' ' &&
-            this._board.TileAt(0, 2)!.Symbol !== ' ') {
+    private isRowFullyOccupied(x: Coordinate) {
+        return this._board.TileAt(x, 0).Symbol !== ' ' &&
+            this._board.TileAt(x, 1).Symbol !== ' ' &&
+            this._board.TileAt(x, 2).Symbol !== ' '
+
+    }
+
+    public Winner(): SymbolType {
+        if (this.isRowFullyOccupied(0)) {
             //if first row is full with same symbol
-            if (this._board.TileAt(0, 0)!.Symbol ===
-                this._board.TileAt(0, 1)!.Symbol &&
-                this._board.TileAt(0, 2)!.Symbol === this._board.TileAt(0, 1)!.Symbol) {
-                return this._board.TileAt(0, 0)!.Symbol;
+            if (this._board.TileAt(0, 0).Symbol ===
+                this._board.TileAt(0, 1).Symbol &&
+                this._board.TileAt(0, 2).Symbol === this._board.TileAt(0, 1).Symbol) {
+                return this._board.TileAt(0, 0).Symbol;
             }
         }
 
-        //if the positions in first row are taken
-        if (this._board.TileAt(1, 0)!.Symbol !== ' ' &&
-            this._board.TileAt(1, 1)!.Symbol !== ' ' &&
-            this._board.TileAt(1, 2)!.Symbol !== ' ') {
+        if (this.isRowFullyOccupied(1)) {
             //if middle row is full with same symbol
-            if (this._board.TileAt(1, 0)!.Symbol ===
-                this._board.TileAt(1, 1)!.Symbol &&
-                this._board.TileAt(1, 2)!.Symbol ===
-                this._board.TileAt(1, 1)!.Symbol) {
-                return this._board.TileAt(1, 0)!.Symbol;
+            if (this._board.TileAt(1, 0).Symbol ===
+                this._board.TileAt(1, 1).Symbol &&
+                this._board.TileAt(1, 2).Symbol ===
+                this._board.TileAt(1, 1).Symbol) {
+                return this._board.TileAt(1, 0).Symbol;
             }
         }
 
-        //if the positions in first row are taken
-        if (this._board.TileAt(2, 0)!.Symbol !== ' ' &&
-            this._board.TileAt(2, 1)!.Symbol !== ' ' &&
-            this._board.TileAt(2, 2)!.Symbol !== ' ') {
+        if (this.isRowFullyOccupied(2)) {
             //if middle row is full with same symbol
-            if (this._board.TileAt(2, 0)!.Symbol ===
-                this._board.TileAt(2, 1)!.Symbol &&
-                this._board.TileAt(2, 2)!.Symbol ===
-                this._board.TileAt(2, 1)!.Symbol) {
-                return this._board.TileAt(2, 0)!.Symbol;
+            if (this._board.TileAt(2, 0).Symbol ===
+                this._board.TileAt(2, 1).Symbol &&
+                this._board.TileAt(2, 2).Symbol ===
+                this._board.TileAt(2, 1).Symbol) {
+                return this._board.TileAt(2, 0).Symbol;
             }
         }
 
